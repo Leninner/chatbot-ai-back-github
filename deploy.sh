0153a12140f58fd26d08de1d06c4ae5c456a18a4
leninner <mazabandalenin180@gmail.com>
2023-05-09 08:47:07 -0500

    learning devops

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-profile=$1 || "default"
+profile=$1
 docker compose build
 sam build -uc -bi chatbot-ai-back-github-app-node -bi ChatHandlerFunction=chatbot-ai-back-github-app-python 
 sam deploy --config-env dev --profile $profile --no-confirm-changeset --no-fail-on-empty-changeset
