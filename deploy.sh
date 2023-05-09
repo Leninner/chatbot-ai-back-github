b7951f96a34849c2b182172d95e758b3e8d2ad23
leninner <mazabandalenin180@gmail.com>
2023-05-09 08:56:32 -0500

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 profile=$1
+
 docker compose build
 sam build -uc -bi chatbot-ai-back-github-app-node -bi ChatHandlerFunction=chatbot-ai-back-github-app-python 
-sam deploy --config-env dev --profile $profile --no-confirm-changeset --no-fail-on-empty-changeset
+
+if [ -z "$profile" ]; then
+  sam deploy --config-env dev --profile $profile --no-confirm-changeset --no-fail-on-empty-changeset
+else
+  sam deploy --config-env dev --no-confirm-changeset --no-fail-on-empty-changeset
+fi
